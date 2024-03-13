1fbf4a7657626c0142c527cddd4718b267d1e6cb
PRITHIV SAKTHI U R <117709109+PRITHIVSAKTHIUR@users.noreply.github.com>
2024-03-14 00:23:17 +0530

    Add files via upload

---
+++ HomePage.py
@@ -0,0 +1,25 @@
+#import libraries
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(page_title="Hey Sri", page_icon='🎙️', layout="wide")
+st.title("Hey Sri 🎙️")
+st.subheader("// Hands On  Python WorkSpace //")
+st.write('''''')
+st.subheader("Objectives")
+st.markdown("""
+1. Understand python basics.
+2. Learn to write and test Python 3 code.
+3. Dive into more advanced topics of python 3.
+4. Build voice recognition application using the integrated knowledge 
+""")
+st.subheader("Requirements")
+st.markdown("""
+1. Internet Connection
+2 .Min Windows version : 10
+3. Min python version : 3.7.12
+4. Visual Studio Code
+""")
+
+
